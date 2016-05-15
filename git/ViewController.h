303f63e1184d7446753e074cbdc68faa7c5b1725
宫玉成 <gary@gongyuchengdeMacBook-Pro.local>
2016-05-15 17:36:38 +0800

    one

---
+++ git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git
+//
+//  Created by 宫玉成 on 16/5/15.
+//  Copyright © 2016年 宫玉成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ git/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  git
+//
+//  Created by 宫玉成 on 16/5/15.
+//  Copyright © 2016年 宫玉成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
